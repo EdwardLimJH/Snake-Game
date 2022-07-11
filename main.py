ed503c5241615454fc0064de414aa5ed2bfa3c21
Edward Lim Jun Heng <edwardlim026@gmail.com>
2022-07-11 14:30:21 +0800

    Added game over mechanics and images

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
 import pygame
-from constant import *
-from objects import *
+import constant as cst
+import objects as obj
 
 pygame.init()
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+SCREEN = pygame.display.set_mode((cst.SCREEN_WIDTH, cst.SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
 
 def game_loop():
-    snake = Snake()
-    apple = Apple()
+    snake = obj.Snake()
+    apple = obj.Apple()
+    background_img = pygame.image.load(cst.BACKGROUND_IMG_FILEPATH)
+    background_img = pygame.transform.scale(background_img,
+                                            (cst.SCREEN_WIDTH,
+                                             cst.SCREEN_HEIGHT))
+
     apple.spawn(snake)
     run = True
+
     while run:
+        clock.tick(cst.FRAME_RATE)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.KEYDOWN:
-                #check if can move in specified direction.
                 if event.key == pygame.K_UP:
-                    snake.move_up(apple)
-                    snake.hit_boundary()
+                    snake.move_up()
+
                 elif event.key == pygame.K_DOWN:
-                    snake.move_down(apple)
-                    snake.hit_boundary()
+                    snake.move_down()
+
                 elif event.key == pygame.K_LEFT:
-                    snake.move_left(apple)
-                    snake.hit_boundary()
+                    snake.move_left()
+
                 elif event.key == pygame.K_RIGHT:
-                    snake.move_right(apple)
-                    snake.hit_boundary()
-        SCREEN.fill("white")
-        snake.draw_snake(SCREEN)
-        apple.draw_apple(SCREEN)
-        pygame.display.update()
+                    snake.move_right()
+
+        snake.auto_move(apple)
+        snake.check_game_over(apple)
+        draw_display(SCREEN, snake, apple, background_img)
     pygame.quit()
+
+
+def draw_display(SCREEN, snake, apple, background_img):
+    SCREEN.blit(background_img, (0, 0))
+    snake.draw_snake(SCREEN)
+    apple.draw_apple(SCREEN)
+    pygame.display.update()
 
 
 if __name__ == "__main__":
--- objects.py
+++ objects.py
@@ -1,51 +1,65 @@
 import random as rand
 import pygame
-from constant import *
+import constant as cst
+
 
 class Snake:
 
     def __init__(self) -> None:
-        self.body = [pygame.Rect(rand.randint(0,(SCREEN_WIDTH//SNAKE_BODY_SIZE)-1)*SNAKE_BODY_SIZE,
-                     rand.randint(0,(SCREEN_HEIGHT//SNAKE_BODY_SIZE)-1)*SNAKE_BODY_SIZE,
-                     SNAKE_BODY_SIZE,
-                     SNAKE_BODY_SIZE)]
-        self.block_size = SNAKE_BODY_SIZE
+        self.body = [pygame.Rect(rand.randint(
+                                 0, (cst.SCREEN_WIDTH//cst.SNAKE_BODY_SIZE)-1)
+                                 * cst.SNAKE_BODY_SIZE,
+                     rand.randint(
+                        0, (cst.SCREEN_HEIGHT//cst.SNAKE_BODY_SIZE)-1)
+                        * cst.SNAKE_BODY_SIZE,
+                     cst.SNAKE_BODY_SIZE,
+                     cst.SNAKE_BODY_SIZE)]
+        self.block_size = cst.SNAKE_BODY_SIZE
         self.vertical = 0
         self.horizontal = 0
-    
+        self.head_img = pygame.image.load(cst.SNAKE_HEAD_IMG_FILEPATH)
+        self.head_img = pygame.transform.scale(self.head_img,
+                                               (self.block_size*2,
+                                                self.block_size*2))
+
     def get_body(self):
         return self.body
-    
+
     def get_block_size(self):
         return self.block_size
 
     def get_horizontal(self):
         return self.horizontal
-    
+
     def get_vertical(self):
         return self.vertical
 
-    def set_body(self,lst):
+    def get_head_image(self):
+        return self.head_img
+
+    def set_body(self, lst):
         self.body = lst
-    
-    def set_horizontal(self,num):
+
+    def set_horizontal(self, num):
         self.horizontal = num
 
-    def set_vertical(self,num):
+    def set_vertical(self, num):
         self.vertical = num
 
-    def create_snake_block(self,x,y):
+    def create_snake_block(self, x, y):
         return pygame.Rect(x, y, self.get_block_size(), self.get_block_size())
 
-    def set_directions(self,horizontal,vertical):
+    def set_directions(self, horizontal, vertical):
         self.set_horizontal(horizontal)
         self.set_vertical(vertical)
 
     def add_head(self):
         old_body = self.get_body()
         head = old_body[0]
-        new_head = self.create_snake_block(head.x + self.get_horizontal()*self.get_block_size(),
-                                    head.y + self.get_vertical()*self.get_block_size())
+        new_head = self.create_snake_block(head.x + self.get_horizontal()
+                                           * self.get_block_size(),
+                                           head.y + self.get_vertical()
+                                           * self.get_block_size())
         self.set_body([new_head] + old_body)
 
     def remove_tail(self):
@@ -53,56 +67,30 @@ class Snake:
         old_body.pop(-1)
         self.set_body(old_body)
 
-    def move_left(self,apple):
-        if self.get_horizontal() == 1 and len(self.get_body()) != 1:
-            pass
+    def auto_move(self, apple):
+        self.add_head()
+        if self.eat_apple(apple):
+            apple.spawn(self)
         else:
-            self.set_directions(-1,0)
-            self.add_head()
+            self.remove_tail()
 
-            if self.eat_apple(apple):
-                apple.spawn(self)
-            else:
-                self.remove_tail()
+    def move_left(self):
+        if not(self.get_horizontal() == 1 and len(self.get_body()) != 1):
+            self.set_directions(-1, 0)
 
-    def move_right(self, apple):
-        if self.get_horizontal() == -1 and len(self.get_body()) != 1:
-            pass
-        else:
-            self.set_directions(1,0)
-            self.add_head()
+    def move_right(self):
+        if not (self.get_horizontal() == -1 and len(self.get_body()) != 1):
+            self.set_directions(1, 0)
 
-            if self.eat_apple(apple):
-                apple.spawn(self)
-            else:
-                self.remove_tail()
+    def move_up(self):
+        if not (self.get_vertical() == 1 and len(self.get_body()) != 1):
+            self.set_directions(0, -1)
 
+    def move_down(self):
+        if not(self.get_vertical() == -1 and len(self.get_body()) != 1):
+            self.set_directions(0, 1)
 
-    def move_up(self,apple):
-        if self.get_vertical() == 1 and len(self.get_body()) != 1:
-            pass
-        else:
-            self.set_directions(0,-1)
-            self.add_head()
-
-            if self.eat_apple(apple):
-                apple.spawn(self)
-            else:
-                self.remove_tail()
-
-    def move_down(self,apple):
-        if self.get_vertical() == -1 and len(self.get_body()) != 1:
-            pass
-        else:
-            self.set_directions(0,1)
-            self.add_head()
-
-            if self.eat_apple(apple):
-                apple.spawn(self)
-            else:
-                self.remove_tail()
-
-    def eat_apple(self,apple):
+    def eat_apple(self, apple):
         apple_rect = apple.get_body()
         head = self.get_body()[0]
         if head.colliderect(apple_rect):
@@ -112,44 +100,82 @@ class Snake:
 
     def hit_boundary(self):
         head = self.get_body()[0]
-        if head.x < 0 or head.x >= SCREEN_WIDTH or head.y < 0 or head.y >= SCREEN_HEIGHT:
-            rect = self.create_snake_block(250,250)
-            self.set_body([rect])
+        exceed_width = head.x < 0 or head.x >= cst.SCREEN_WIDTH
+        exceed_height = head.y < 0 or head.y >= cst.SCREEN_HEIGHT
+        if exceed_width or exceed_height:
+            return True
+        return False
 
-    def draw_snake(self,screen):
-        for rect in self.get_body():
-            pygame.draw.rect(screen,"red",rect)
+    def hit_self(self):
+        body = self.get_body()
+        head = body[0]
+        return any([head.colliderect(x) for x in body[1:]])
+
+    def check_game_over(self, apple):
+        if self.hit_boundary() or self.hit_self():
+            self.restart()
+            apple.restart(self)
+
+    def restart(self):
+        self.set_directions(0, 0)
+        rect = self.create_snake_block(cst.SNAKE_RESPAWN_X,
+                                       cst.SNAKE_RESPAWN_Y)
+        self.set_body([rect])
+
+    def draw_snake(self, screen):
+        for indx, rect in enumerate(self.get_body()):
+            if indx == 0:
+                screen.blit(self.get_head_image(), (rect.x, rect.y),
+                            (12.5, 12.5, 25, 25))
+            else:
+                pygame.draw.rect(screen, "yellow", rect)
 
 
 class Apple:
     def __init__(self):
         self.body = None
-        self.block_size = APPLE_SIZE
+        self.block_size = cst.APPLE_SIZE
+        self.image = pygame.image.load(cst.APPLE_IMG_FILEPATH)
+        self.image = pygame.transform.scale(self.image, (200, 200))
 
     def get_body(self):
         return self.body
 
-    def set_body(self,rect):
+    def set_body(self, rect):
         self.body = rect
 
     def get_block_size(self):
         return self.block_size
 
-    def create_apple_rect(self,x,y):
-        return pygame.Rect(x, y,self.get_block_size(), self.get_block_size())
-       
-    def spawn(self,snake):
+    def get_image(self):
+        return self.image
+
+    def create_apple_rect(self, x, y):
+        return pygame.Rect(x, y, self.get_block_size(), self.get_block_size())
+
+    def spawn(self, snake):
         snake_body = snake.get_body()
-        occupied = set(map(lambda rect: (rect.x,rect.y),snake_body))
-        loc = (rand.randint(0,(SCREEN_WIDTH//APPLE_SIZE)-1)*APPLE_SIZE,
-               rand.randint(0,(SCREEN_HEIGHT//APPLE_SIZE)-1)*APPLE_SIZE)
+        occupied = set(map(lambda rect: (rect.x, rect.y), snake_body))
+        loc = (rand.randint(0, (cst.SCREEN_WIDTH//self.get_block_size()) - 1) *
+               self.get_block_size(),
+               rand.randint(0, (cst.SCREEN_HEIGHT//self.get_block_size()) - 1)
+               * self.get_block_size())
         while True:
             if loc not in occupied:
                 break
-            loc = (rand.randint(0,(SCREEN_WIDTH//APPLE_SIZE)-1)*APPLE_SIZE,
-                   rand.randint(0,(SCREEN_HEIGHT//APPLE_SIZE)-1)*APPLE_SIZE)
-        rect = self.create_apple_rect(loc[0],loc[1])
+            loc = (rand.randint(0,
+                   (cst.SCREEN_WIDTH//self.get_block_size()) - 1)
+                   * self.get_block_size(),
+                   rand.randint(0,
+                   (cst.SCREEN_HEIGHT//self.get_block_size())-1)
+                   * self.get_block_size())
+        rect = self.create_apple_rect(loc[0], loc[1])
         self.set_body(rect)
-    
-    def draw_apple(self,screen):
-        pygame.draw.rect(screen,"green",self.get_body())
+
+    def restart(self, snake):
+        self.spawn(snake)
+
+    def draw_apple(self, screen):
+        loc = self.get_body()
+        screen.blit(self.get_image(), (loc.x, loc.y), 
+                    (9, 9, self.get_block_size(), self.get_block_size()))
